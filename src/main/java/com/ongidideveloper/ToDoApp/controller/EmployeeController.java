c4865e6acf03b22fba842efe419b37b30a590643
ONGIDI E. GEOFREY <ongidigeofrey@gmail.com>
2023-09-23 05:50:25 -0700

    UPDATE EMPLOYEE DETAILS

--- src/main/java/com/ongidideveloper/ToDoApp/controller/EmployeeController.java
+++ src/main/java/com/ongidideveloper/ToDoApp/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.ongidideveloper.ToDoApp.controller;
 
 
+import com.ongidideveloper.ToDoApp.exception.ResourceNotFoundException;
 import com.ongidideveloper.ToDoApp.model.Employee;
 import com.ongidideveloper.ToDoApp.repository.EmployeeRepository;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +27,23 @@ public class EmployeeController {
     @PostMapping("create")
     public Employee create(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+    //GetById
+
+    @PostMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable  long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+        return ResponseEntity.ok(employee);
+    }
+    @PutMapping("edit/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable  long id,@RequestBody Employee employeeDetails){
+        Employee  updateEmployee= employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailID(employeeDetails.getEmailID());
+        employeeRepository.save(updateEmployee);
+        return ResponseEntity.ok(updateEmployee);
     }
 }
--- src/main/java/com/ongidideveloper/ToDoApp/exception/ResourceNotFoundException.java
+++ src/main/java/com/ongidideveloper/ToDoApp/exception/ResourceNotFoundException.java
@@ -10,4 +10,5 @@ public class ResourceNotFoundException extends RuntimeException {
         super(message);
 
     }
+
 }
